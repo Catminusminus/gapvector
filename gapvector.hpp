24290fbd231bb9cc017786273fa583d7f4431dda
Catminusminus <getomya@svk.jp>
2018-04-22 22:30:57 +0900

    Implement front and back

--- gapvector.hpp
+++ gapvector.hpp
@@ -7,7 +7,7 @@
 
 namespace my
 {
-constexpr int N = 10;
+constexpr size_t N = 10;
 
 template <typename T>
 class gapvectorIterator;
@@ -25,25 +25,28 @@ public:
 
 private:
   std::vector<T> inner_vector;
-  int gap_begin = 0;
-  int gap_last = N - 1;
-  void format_gapvector(int);
+  size_t gap_begin = 0;
+  size_t gap_last = N - 1;
+  void format_gapvector(size_t);
   void gap_alloc();
 
 public:
   gapvector();
-  void insert(int, const T &);
-  void insert(int, T &&);
+  void insert(size_t, const T &);
+  void insert(size_t, T &&);
   iterator insert(iterator, const T &);
   void push_back(const T &);
   void push_back(T &&);
-  void erase(int);
+  void erase(size_t);
   iterator erase(iterator);
   void pop_back();
   void clear();
   int size();
-  T &operator[](int);
-  T &at(int);
+  T &operator[](size_t);
+  T &at(size_t);
+  T &front();
+  T &back();
+  T *data();
 };
 #include "gapvector_implement.hpp"
 };
